6dce6615ca5f1d94f208d661449c89e050bbbe48
70n1 <el.70n1@gmail.com>
2017-02-21 11:16:47 +0100

    Restringir el envio de notificaciones mediante temas

--- app/src/main/java/org/example/eventos/Temas.java
+++ app/src/main/java/org/example/eventos/Temas.java
@@ -7,8 +7,10 @@ import android.support.v7.app.AppCompatActivity;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
 
+import com.google.firebase.iid.FirebaseInstanceId;
 import com.google.firebase.messaging.FirebaseMessaging;
 
+import static org.example.eventos.EventosFCMInstanceIDService.guardarIdRegistro;
 import static org.example.eventos.EventosFCMService.mostrarDialogo;
 
 /**
@@ -21,6 +23,8 @@ public class Temas extends AppCompatActivity {
     CheckBox checkBoxCine;
     CheckBox checkBoxFiestas;
 
+    CheckBox checkBoxNoRecibirNotificaciones;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,6 +33,13 @@ public class Temas extends AppCompatActivity {
         checkBoxTeatro = (CheckBox) findViewById(R.id.checkBoxTeatro);
         checkBoxCine = (CheckBox) findViewById(R.id.checkBoxCine);
         checkBoxFiestas = (CheckBox) findViewById(R.id.checkBoxFiestas);
+        checkBoxNoRecibirNotificaciones = (CheckBox) findViewById(R.id.checkBoxNoRecibirNotificaciones);
+        Boolean noRecibirNotificaciones = consultarSuscripcionATemaEnPreferencias(getApplicationContext(), "Todos");
+        checkBoxNoRecibirNotificaciones.setChecked(noRecibirNotificaciones);
+        checkBoxDeportes.setEnabled(!noRecibirNotificaciones);
+        checkBoxTeatro.setEnabled(!noRecibirNotificaciones);
+        checkBoxCine.setEnabled(!noRecibirNotificaciones);
+        checkBoxFiestas.setEnabled(!noRecibirNotificaciones);
         checkBoxDeportes.setChecked(consultarSuscripcionATemaEnPreferencias(getApplicationContext(), "Deportes"));
         checkBoxTeatro.setChecked(consultarSuscripcionATemaEnPreferencias(getApplicationContext(), "Teatro"));
         checkBoxCine.setChecked(consultarSuscripcionATemaEnPreferencias(getApplicationContext(), "Cine"));
@@ -53,19 +64,43 @@ public class Temas extends AppCompatActivity {
                 mantenimientoSuscripcionesATemas("Fiestas", isChecked);
             }
         });
+
+        checkBoxNoRecibirNotificaciones.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                mantenimientoSuscripcionesATemas("Todos", isChecked);
+            }
+        });
     }
 
     private void mantenimientoSuscripcionesATemas(String tema, Boolean suscribir) {
-        if (suscribir) {
-            mostrarDialogo(getApplicationContext(), "Te has suscrito a: " + tema);
-            FirebaseMessaging.getInstance().subscribeToTopic(tema);
-            guardarSuscripcionATemaEnPreferencias(getApplicationContext(), tema, true);
+        if (tema.equals("Todos")) {
+            if (suscribir) {
+                FirebaseMessaging.getInstance().unsubscribeFromTopic(tema);
+                guardarSuscripcionATemaEnPreferencias(getApplicationContext(), tema, true);
+                checkBoxDeportes.setChecked(false);
+                checkBoxTeatro.setChecked(false);
+                checkBoxCine.setChecked(false);
+                checkBoxFiestas.setChecked(false);
+            } else {
+                FirebaseMessaging.getInstance().subscribeToTopic(tema);
+                guardarIdRegistro(getApplicationContext(), FirebaseInstanceId.getInstance().getToken());
+                guardarSuscripcionATemaEnPreferencias(getApplicationContext(), tema, false);
+            }
+            checkBoxDeportes.setEnabled(!suscribir);
+            checkBoxTeatro.setEnabled(!suscribir);
+            checkBoxCine.setEnabled(!suscribir);
+            checkBoxFiestas.setEnabled(!suscribir);
         } else {
-            mostrarDialogo(getApplicationContext(), "Te has dado de baja de: " + tema);
-            FirebaseMessaging.getInstance().unsubscribeFromTopic(tema);
-            guardarSuscripcionATemaEnPreferencias(getApplicationContext(), tema, false);
+            if (suscribir) {
+                FirebaseMessaging.getInstance().subscribeToTopic(tema);
+                guardarSuscripcionATemaEnPreferencias(getApplicationContext(), tema, true);
+            } else {
+                FirebaseMessaging.getInstance().unsubscribeFromTopic(tema);
+                guardarSuscripcionATemaEnPreferencias(getApplicationContext(), tema, false);
+            }
         }
     }
+
 
     public static void guardarSuscripcionATemaEnPreferencias(Context context, String tema, Boolean suscrito) {
         final SharedPreferences prefs = context.getSharedPreferences("Temas", Context.MODE_PRIVATE);
