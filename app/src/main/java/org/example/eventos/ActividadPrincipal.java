832a8606a3c8ea0e866813617d7104bfbebbbaa4
70n1 <el.70n1@gmail.com>
2017-02-21 11:01:51 +0100

    Suscribir y dar de baja a usuarios en temas en FCM

--- app/src/main/java/org/example/eventos/ActividadPrincipal.java
+++ app/src/main/java/org/example/eventos/ActividadPrincipal.java
@@ -1,9 +1,13 @@
 package org.example.eventos;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.Toast;
 
 import com.firebase.ui.database.FirebaseRecyclerAdapter;
@@ -24,6 +28,8 @@ public class ActividadPrincipal extends AppCompatActivity {
     RecyclerView recyclerView;
     private DatabaseReference databaseReference;
     private FirebaseRecyclerAdapter adapter;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -41,7 +47,10 @@ public class ActividadPrincipal extends AppCompatActivity {
                 databaseReference);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
         recyclerView.setAdapter(adapter);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
     }
+
     private boolean comprobarGooglePlayServices() {
         int resultCode = GooglePlayServicesUtil
                 .isGooglePlayServicesAvailable(this);
@@ -56,11 +65,13 @@ public class ActividadPrincipal extends AppCompatActivity {
         }
         return true;
     }
+
     @Override
     protected void onStart() {
         super.onStart();
-        current=this;
+        current = this;
     }
+
     public static ActividadPrincipal getCurrentContext() {
         return current;
     }
@@ -69,12 +80,12 @@ public class ActividadPrincipal extends AppCompatActivity {
     protected void onResume() {
         super.onResume();
         Bundle extras = getIntent().getExtras();
-        if (extras!=null && extras.keySet().size()>4) {
-            String evento="";
-            evento ="Evento: "+extras.getString("evento")+ "\n";
-            evento = evento + "Día: "+ extras.getString("dia")+ "\n";
-            evento = evento +"Ciudad: "+extras.getString("ciudad")+ "\n";
-            evento = evento +"Comentario: "+extras.getString("comentario");
+        if (extras != null && extras.keySet().size() > 4) {
+            String evento = "";
+            evento = "Evento: " + extras.getString("evento") + "\n";
+            evento = evento + "Día: " + extras.getString("dia") + "\n";
+            evento = evento + "Ciudad: " + extras.getString("ciudad") + "\n";
+            evento = evento + "Comentario: " + extras.getString("comentario");
             mostrarDialogo(getApplicationContext(), evento);
             for (String key : extras.keySet()) {
                 getIntent().removeExtra(key);
@@ -82,4 +93,22 @@ public class ActividadPrincipal extends AppCompatActivity {
             extras = null;
         }
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_principal, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_temas) {
+            Intent intent = new Intent(getBaseContext(), Temas.class);
+            startActivity(intent);
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
 }
+
