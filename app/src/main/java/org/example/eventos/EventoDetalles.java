c9a6f4313eed2840db06b60798926c0dcb1aaa58
70n1 <el.70n1@gmail.com>
2017-02-23 14:33:53 +0100

    Preparar la aplicación para usar la API de Google Drive.

--- app/src/main/java/org/example/eventos/EventoDetalles.java
+++ app/src/main/java/org/example/eventos/EventoDetalles.java
@@ -62,6 +62,7 @@ public class EventoDetalles extends AppCompatActivity {
     final int SOLICITUD_SUBIR_PUTFILE = 2;
     final int SOLICITUD_SELECCION_STREAM = 100;
     final int SOLICITUD_SELECCION_PUTFILE = 101;
+    final int SOLICITUD_FOTOGRAFIAS_DRIVE = 102;
     private ProgressDialog progresoSubida;
     Boolean subiendoDatos = false;
 
@@ -116,6 +117,11 @@ public class EventoDetalles extends AppCompatActivity {
                 break;
             case R.id.action_deleteFile:
                 borrarFicheroFirebaseStorage();
+                break;
+            case R.id.action_fotografiasDrive:
+                Intent intent = new Intent(getBaseContext(), FotografiasDrive.class);
+                intent.putExtra("evento", evento);
+                startActivity(intent);
                 break;
         }
         return super.onOptionsItemSelected(item);
